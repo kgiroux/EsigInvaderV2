e98665743c63e201a538aac221e24a92381a0bb8
LirycMoaners <cyril.lefebvre.27@gmail.com>
2015-10-07 11:51:42 +0200

    heyhey

--- EsigInvader2/main.cpp
+++ EsigInvader2/main.cpp
@@ -1,6 +1,21 @@
-#include<iostream>
-using namespace std;
-void main() {
-	cout << "Hello world !!!" << endl;
-	system("pause");
+#include <iostream>
+#include "SDL.h"
+
+void main()
+{
+	SDL_Surface *screen;
+	if (SDL_Init(SDL_INIT_VIDEO) == -1)
+	{
+		printf("Can't init SDL:  %s\n", SDL_GetError());
+	}
+
+	atexit(SDL_Quit);
+	screen = SDL_SetVideoMode(640, 480, 16, SDL_HWSURFACE);
+
+	if (screen == NULL)
+	{
+		printf("Can't set video mode: %s\n", SDL_GetError());
+	}
+
+	SDL_Delay(3000);
 }
